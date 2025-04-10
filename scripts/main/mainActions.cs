e183f98f1697ebf91ee4dd46c4843ef20b836c91
Ryan Ferreira <RyanMatheusFerreira@outlook.com.br>
2025-04-10 12:25:03 -0300

    Implementa a lógica de botões em mainActions. Cria a cena de debug_level.

---
+++ scripts/main/mainActions.cs
@@ -0,0 +1,14 @@
+using Godot;
+using System;
+
+public partial class mainActions : Button
+{
+    public void QuitButtonPressed()
+    {
+        GetTree().Quit();
+    }
+    public void StartButtonPressed()
+    {
+        GetTree().ChangeSceneToFile("res://scenes//debug/debug_level.tscn");
+    }
+}
