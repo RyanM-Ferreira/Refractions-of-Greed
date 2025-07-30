4f66ea9f0fc1f878bd9d12c6e05d610e9a01fd70
Ryan Ferreira <RyanMatheusFerreira@outlook.com.br>
2025-07-29 22:45:13 -0300

    Novo menu inicial, ajustes na interface e nova HP Bar

--- scripts/main/mainActions.cs
+++ scripts/main/mainActions.cs
@@ -1,6 +1,4 @@
 using Godot;
-using System;
-using System.Diagnostics;
 
 public partial class mainActions : Button
 {
@@ -13,8 +11,35 @@ public partial class mainActions : Button
 		GetTree().ChangeSceneToFile("res://scenes//Levels/cave/Level_1.tscn");
 	}
 
+	public void OptionsMenuButtonPressed()
+	{
+		//Provavelmente
+	}
+
 	public void DebugButtonPressed()
-		{
+	{
 		GetTree().ChangeSceneToFile("res://scenes//debug/debug_level.tscn");
+	}
+
+	private Vector2 originalPosition;
+
+	public override void _Ready()
+	{
+		originalPosition = Position;
+		MouseEntered += OnMouseEntered;
+		MouseExited += OnMouseExited;
+	}
+
+	private void OnMouseEntered()
+	{
+		Scale = new Vector2(1.125f, 1.125f);
+
+		Position = originalPosition - ((Scale - new Vector2(1,1)) * Size) / 2;
+	}
+
+	private void OnMouseExited()
+	{
+		Scale = new Vector2(1,1);
+		Position = originalPosition;
 	}
 }
