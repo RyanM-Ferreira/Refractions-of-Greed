2adc569f81d464baf8be8a263651c4eae3b1f134
Ryan Ferreira <RyanMatheusFerreira@outlook.com.br>
2025-08-01 22:31:14 -0300

    Primeira Splash Screen

---
+++ scripts/main/SplashScreen.cs
@@ -0,0 +1,20 @@
+using Godot;
+
+public partial class SplashScreen : Node2D
+{
+	public override void _Ready()
+	{
+		Timer timer = GetNode<Timer>("Timer");
+		timer.Timeout += OnTimeout;
+		timer.Start();
+	}
+
+	private void OnTimeout()
+	{
+		GetTree().ChangeSceneToFile("res://scenes/main/main.tscn");
+	}
+
+	public override void _Process(double delta)
+	{
+	}
+}
